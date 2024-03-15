191879de32107c36cb8de52903ae8d50125d516e
karencarvalho04 <karen_carvalho04@hotmail.com>
2024-03-14 21:52:20 -0300

    atualização layout

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ function nextImage() {
 
 }
 
+
+let meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+let data = new Date();
+let diaNumero = data.getDay();
+let dia = data.getDate();
+let mes = mes[data.getMonth()];
+let ano = data.getFullYear();
+
+let ativo= document.querySelector(".semana li:nth-child("+diaNumero+")")
+ativo.classList.add('atual'); 
+
+let h1 = document.createElement('h1');
+h1.innerHTML = dia;
+ativo.appendChild(h1);
+
+let h5 = document.createElement('h5');
+h1.innerHTML = mes;
+ativo.appendChild(h5);
+
+let h3 = document.createElement('h3');
+h1.innerHTML = ano;
+ativo.appendChild(h3);
