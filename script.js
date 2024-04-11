90a3172dab87767eb62ed2460e20f71413a5ef5e
karencarvalho04 <karen_carvalho04@hotmail.com>
2024-04-10 21:45:59 -0300

    atualizações resp 10.04

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
 
-setInterval(function () {
+setInterval(function(){
     nextImage();
-}, 5000)
+}, 8000)
 
-function nextImage() {
-
+function nextImage(){
     count++;
-    if (count > 7) {
+    if(count > 7){
         count = 1;
     }
 
     document.getElementById("radio" + count).checked = true;
-
 }
 
 
