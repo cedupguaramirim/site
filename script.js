6b47b79291914a758532eff8bcfb44cc114bfbbb
karencarvalho04 <karen_carvalho04@hotmail.com>
2024-04-12 16:36:07 -0300

    atualizações resp 11.04

--- script.js
+++ script.js
@@ -38,5 +38,22 @@ let h3 = document.createElement('h3');
 h1.innerHTML = ano;
 ativo.appendChild(h3);
 
+const tabs = document.querySelectorAll('.dropbtn');
 
+tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
 
+const tabClicked = (tab) => {
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tab.classList.add('active');
+
+    const contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.classList.remove('show'));
+
+    const contentId = tab.getAttribute('content-id');
+    const content = document.getElementById(contentId);
+
+    content.classList.add('show');
+}
+
+const currentActiveTab = document.querySelector('.tab-btn.active');
+tabClicked(currentActiveTab);
